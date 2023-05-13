f5f76670ebc0ab2e426bcfc03fa536083caf6e5a
rivaldy <frizgt@gmail.com>
2023-05-13 16:21:17 +0800

    :label: make ProfileScreen to separate screen

--- app/src/main/java/id/rivaldy/composeapp/ui/screen/profile/ProfileScreen.kt
+++ app/src/main/java/id/rivaldy/composeapp/ui/screen/profile/ProfileScreen.kt
@@ -1,16 +1,11 @@
 package id.rivaldy.composeapp.ui.screen.profile
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
@@ -19,13 +14,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import id.rivaldy.composeapp.ui.screen.profile.section.ProfileContent
 import id.rivaldy.core.R
 import id.rivaldy.core.ui.theme.Gray200
 
@@ -62,49 +53,4 @@ fun ProfileScreen() {
                 }
             }
         })
-}
-
-@Composable
-fun ProfileContent() {
-    Card(
-        modifier = Modifier.size(160.dp),
-        shape = CircleShape,
-        elevation = 3.dp
-    ) {
-        Image(
-            painterResource(R.drawable.rival_profile),
-            contentDescription = stringResource(R.string.about_page),
-            contentScale = ContentScale.Crop,
-            modifier = Modifier.fillMaxSize()
-        )
-    }
-    Spacer(modifier = Modifier.size(16.dp))
-    Column(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(
-            text = stringResource(R.string.rivaldy),
-            style = MaterialTheme.typography.subtitle1.copy(
-                fontWeight = FontWeight.Normal, fontSize = 28.sp, textAlign = TextAlign.Center
-            ),
-            color = Color.Black
-        )
-        Spacer(modifier = Modifier.size(3.dp))
-        Text(
-            text = stringResource(R.string.rival_mail),
-            style = MaterialTheme.typography.subtitle2.copy(
-                fontWeight = FontWeight.Light, fontSize = 18.sp, textAlign = TextAlign.Center
-            ),
-            color = Color.DarkGray
-        )
-        Spacer(modifier = Modifier.size(16.dp))
-        Text(
-            text = stringResource(R.string.job_description),
-            style = MaterialTheme.typography.subtitle2.copy(
-                fontWeight = FontWeight.Light, fontSize = 20.sp, textAlign = TextAlign.Center
-            ),
-            color = MaterialTheme.colors.secondary
-        )
-    }
 }
